8120aa8f26ad639f6a709617a7a633feee38b07a
simshadows <contact@simshadows.com>
2026-06-13 22:38:45 +1000

    discord-message-archive: Add dotenv support

--- scripts/discord-message-archive/main.py
+++ scripts/discord-message-archive/main.py
@@ -1,6 +1,7 @@
-import discord
+from dotenv import dotenv_values
+from discord import Client, Intents
 
-class MyClient(discord.Client):
+class ArchiverBot(Client):
     async def on_ready(self):
         print("Logged on as", self.user)
 
@@ -12,11 +13,12 @@ class MyClient(discord.Client):
         if message.content == "ping":
             await message.channel.send("pong")
 
-def main():
-    intents = discord.Intents.default()
+def main(discord_api_key):
+    intents = Intents.default()
     intents.message_content = True
-    client = MyClient(intents=intents)
-    client.run("TODO_PUT_YOUR_TOKEN_HERE")
+    client = ArchiverBot(intents=intents)
+    client.run(discord_api_key)
 
 if __name__ == "__main__":
-    main()
+    discord_api_key = dotenv_values()["DISCORD_API_KEY"]
+    main(discord_api_key)
